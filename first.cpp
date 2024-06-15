c550256ad1a98cc647a45d77623c12bcff079b66
Isaac  Appiagyei <ikwaku465@gmail.com>
2024-06-15 19:02:36 +0200

    Add files via upload
    Added some comments to make the code more readable

--- first.cpp
+++ first.cpp
@@ -16,6 +16,7 @@ void askAndAddUserNumber();
 
 int main() 
 {
+	// Asks user to enter two numbers, adds and print it out
 	askAndAddUserNumber();
 
 	return 0;
@@ -24,20 +25,13 @@ int main()
 
 int myAdditionFunction(int a, int b) 
 {
-	if (a > b)
-	{
-		cout << "Your first number was huuge than the second" << endl;
-	}
-	else
-	{
-		cout << "Your second number was a big one" << endl;
-	}
+	// Adds the two integers from the user
 	return a + b;
-
 }
 
 int askUser1() 
 {
+	// Asks the user to enter some nubers of choices 
 	cout << "Enter first number : " << endl;
 
 	int firstNumber;
@@ -48,6 +42,7 @@ int askUser1()
 
 int askUser2()
 {
+	// Asks the user to enter another nubers of choices 
 	cout << "Enter second number : " << endl;
 
 	int secondNumber;
@@ -60,12 +55,22 @@ int askUser2()
 
 void printRespond(int first, int second) 
 {
+	// This function uses the addition function and print the answer to the user
 	cout << "The addition of both your numbers: " << myAdditionFunction(first, second) << endl;
+	if (first > second)
+	{
+		cout << "Your first number was huuge than the second" << endl;
+	}
+	else
+	{
+		cout << "Your second number was a big one" << endl;
+	}
 	
 }
 
 void askAndAddUserNumber()
 {
+	// Last of the refactoring, puts all the fuctions into one function and makes it more tidy on the Main.
 
 	int firstRespond = askUser1();
 	int secondRespond = askUser2();
